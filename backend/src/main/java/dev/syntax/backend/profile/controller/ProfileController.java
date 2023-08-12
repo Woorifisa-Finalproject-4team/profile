6cbfe4097b6c9c0038e2d9f301087a5ab13e56d2
juneheel <junehee107@naver.com>
2023-08-13 01:07:26 +0900

    setting : vue, bootstrap 연동

--- backend/src/main/java/dev/syntax/backend/profile/controller/ProfileController.java
+++ backend/src/main/java/dev/syntax/backend/profile/controller/ProfileController.java
@@ -6,11 +6,9 @@ import dev.syntax.backend.profile.model.User;
 import dev.syntax.backend.profile.service.ProfileService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class ProfileController {
 
