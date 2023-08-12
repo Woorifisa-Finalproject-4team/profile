a42083a209ca88ad107283d813a319791cc2e5e9
juneheel <junehee107@naver.com>
2023-08-13 01:00:37 +0900

    setting : vue와 bootstrap연동

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,17 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 import router from './router'
+import axios from 'axios'
 
+
+axios.defaults.baseURL = "http://localhost:8080";
 const app = createApp(App)
+app.config.globalProperties.axios = axios;
 
+app.use(bootstrap)
 app.use(createPinia())
 app.use(router)
 
