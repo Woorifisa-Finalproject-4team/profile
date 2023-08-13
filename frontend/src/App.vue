fbedd128b3f9b597edc9bf3af7d3645731165616
leejunehee <junehee107@naver.com>
2023-08-14 01:41:34 +0900

    feat : 페이지 추가 및 라우터 설정 - 꼼수부리지말고 그냥 만들자..

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,64 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import axios from 'axios'
 import {ref} from 'vue'
 
-const userName = ref(0);
-const userId = ref(0);
-const servicePurpose = ref(0);
-const userAgeGroup = ref(0);
-const asset = ref(0);
-
-
- axios.get("http://localhost:8080/1/profile")
-    .then(response => {
-      // Get 요청 성공 시 로직
-      console.log(response.data);
-      // 서버로부터 받은 결과 값을 표시
-      userName.value = response.data.name;
-      userId.value = response.data.userId;
-      // servicePurpose.value = response.data.servicePurpose;
-      if(response.data.servicePurpose == 'FORCAR'){
-        servicePurpose.value = '자동차 사기';
-      }
-      if(response.data.userAgeGroup = 'THREE'){
-        userAgeGroup.value = '30대';
-      } 
-      asset.value = response.data.asset;
-    })
-    // POST 요청 실패 시 로직
-    .catch(error => {
-      console.error(error);
-    });
-
 </script>
-
 <template>
-<div class="container">
- 
-<div class="card mb-3" style="max-width: 800px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="https://picsum.photos/id/873/600/850" class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">{{userName}} 고객님 환영합니다!</h5>
-        <p class="card-text">{{userId}}</p>
-        <p class="card-text">이용목적 : {{servicePurpose}}</p>
-        <p class="card-text">연령대 : {{userAgeGroup}}</p>
-        <p class="card-text">현재 자산 : {{asset}} </p>
-        <button type="button" class="btn btn-outline-info">Edit</button>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-</div>
-
-</div>
-
-<!--
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
@@ -67,13 +13,13 @@ const asset = ref(0);
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <!-- <RouterLink to="/about">About</RouterLink> -->
       </nav>
     </div>
     
   </header>
   <RouterView />
--->
+
   
 </template>
 
